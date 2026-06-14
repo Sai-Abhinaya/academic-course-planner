be226e2e525404db169806eb9836a686bd73fbc3
Sai-Abhinaya <saiabhinayagudivada@gmail.com>
2026-06-14 19:25:08 +0530

    Added Spring Boot course service

---
+++ course-service/src/test/java/com/academicplanner/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
